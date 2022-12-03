fb5e94266f76ec25458eb0f0e62b8dd1b94437c3
Bina Basiri <bina712010@gmail.com>
2022-12-03 18:57:37 -0500

    day2 part2 test added

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const { dayOnePartOne } = require('./day1_part1.js');
 const { dayOnePartTwo } = require('./day1_part2.js');
 const { dayTwoPartOne } = require('./day2_part1.js');
+const { dayTwoPartTwo } = require('./day2_part2.js');
 
 const testFunction = async (fn, dataPath, expectedResult) => {
   const result = await fn(dataPath);
@@ -11,6 +12,7 @@ const dayOnePartOneAnswer = 1581;
 const dayOnePartTwoAnswer = 1618;
 const dayTwoData = 'data_day_2.txt';
 const dayTwoPartOneAnswer = 1654760;
+const dayTwoPartTwoAnswer = 1956047400;
 
 testFunction(dayOnePartOne, dayOneData, dayOnePartOneAnswer).then(
   (testResult) => console.log(testResult)
@@ -19,5 +21,8 @@ testFunction(dayOnePartTwo, dayOneData, dayOnePartTwoAnswer).then(
   (testResult) => console.log(testResult)
 );
 testFunction(dayTwoPartOne, dayTwoData, dayTwoPartOneAnswer).then(
+  (testResult) => console.log(testResult)
+);
+testFunction(dayTwoPartTwo, dayTwoData, dayTwoPartTwoAnswer).then(
   (testResult) => console.log(testResult)
 );

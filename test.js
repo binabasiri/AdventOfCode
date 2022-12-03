445086c2c5b3b7a4e8d554d70496281ef02af21a
Bina Basiri <bina712010@gmail.com>
2022-12-03 18:42:11 -0500

    test function updated, day1 part2 added

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
 const { dayOnePartOne } = require('./day1_part1.js');
+const { dayOnePartTwo } = require('./day1_part2.js');
 
 const testFunction = async (fn, dataPath, expectedResult) => {
   const result = await fn(dataPath);
+  console.log(result);
   return expectedResult === result ? 'PASS' : 'FAIL';
 };
 const dayOneData = 'data_day_1.txt';
 const dayOnePartOneAnswer = 1581;
+const dayOnePartTwoAnswer = 1618;
+
 testFunction(dayOnePartOne, dayOneData, dayOnePartOneAnswer).then(
+  (testResult) => console.log(testResult)
+);
+testFunction(dayOnePartTwo, dayOneData, dayOnePartTwoAnswer).then(
   (testResult) => console.log(testResult)
 );
